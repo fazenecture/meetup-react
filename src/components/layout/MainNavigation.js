026fe6b5fce480c726e82901cbc8d3b4db5ce7f4
Vivek <vivekkumarsingh2002@gmail.com>
2022-05-23 01:31:29 +0530

    navigationBar

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import classes from './MainNaivgation.module.css';
+
 
 function MainNavigation() {
   return (
-    <header>
-      <div>React Header</div>
+    <header className={ classes.header }>
+      <div className={ classes.logo }>React Header</div>
       <nav>
         <ul>
           <li>
